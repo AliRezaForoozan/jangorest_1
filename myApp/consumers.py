bddeeb57bbd62ad352e6c17a180a29980dca2195
Ali Forouzan <designerafg@gmail.com>
2026-02-11 18:19:21 +0330

    add consumer

---
+++ myApp/consumers.py
@@ -0,0 +1,9 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+class StatusConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+
+    async def receive(self, text_data):
+        await self.send(text_data=json.dumps({"reply": "ok"}))
